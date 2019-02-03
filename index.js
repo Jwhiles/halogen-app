cea726e3dfb463195b3ca0c9566e8cf77bdf7a36
John Whiles <jwhiles@johns-air-2.mynet>
2019-02-03 18:11:59 +0000

    get halogen to render something

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 import { main } from './output/Main/index.js';
 
-console.log(main);
-console.log('exist');
+main();
--- src/Main.purs
+++ src/Main.purs
@@ -1,9 +1,39 @@
 module Main where
 
+import Halogen (liftAff, liftEffect)
+import Halogen as H
+import Halogen.Aff as HA
+import Halogen.HTML as HH
+import Data.Maybe (Maybe(..))
+import Effect (Effect(..))
+import Halogen.VDom.Driver (runUI)
+
 import Prelude
-import Effect (Effect)
-import Effect.Console (log)
 
 main :: Effect Unit
-main = do
-   pure unit
+main = HA.runHalogenAff do
+  body <- HA.awaitBody
+  runUI component unit body
+
+data SomeThing a = SomeThing a
+type State = Boolean
+
+component :: forall m. H.Component HH.HTML SomeThing Unit Void m
+component =
+  H.component
+    { initialState: const initialState -- functiomn taking props maybe??
+    , render
+    , eval
+    , receiver: const Nothing
+    }
+  where
+  initialState :: State
+  initialState = false
+
+  render :: State -> H.ComponentHTML SomeThing
+  render state =
+    HH.div_
+    [ HH.h1_ [ HH.text "text renderer in halogen. wow" ] ]
+
+  eval :: SomeThing ~> H.ComponentDSL State SomeThing Void m
+  eval (SomeThing a) = pure a
